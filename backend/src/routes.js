40f6a165a36540041c1658d4855ca86e42123a8f
Pedro Sampaio <pedroigorjs@gmail.com>
2020-03-13 14:34:46 -0300

    Create routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,16 @@
 import { Router } from 'express';
+import DevController from './controllers/DevController';
+import SearchController from './controllers/SearchController';
 
 const routes = new Router();
+
+routes.get('/devs', DevController.index);
+routes.get('/search', SearchController.index);
+
+routes.post('/devs', DevController.store);
+
+routes.put('/devs/:id', DevController.update);
+
+routes.delete('/devs/:id', DevController.destroy);
 
 export default routes;
